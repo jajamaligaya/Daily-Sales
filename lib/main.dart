5489390b3cc62fda99fde483411e15ae3ea56531
myusername <myemail>
2025-06-19 10:24:48 +0800

    activity2

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,43 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title:Text('Daily Sales'),
-        backgroundColor: Colors.teal[200],
-      ),
-      body:Center( // <-- centers horizontally
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('You', style: TextStyle(
-              fontSize: 30,
-              backgroundColor: Colors.pink[100],
-              fontFamily: 'Times New Roman',
-            )
+        title: Text('DailySales'),
+        backgroundColor: Colors.tealAccent[100],
+      ) ,
+      body: Column(
+        children: [
+          Container(
+            margin:EdgeInsets.all(10),
+            padding:EdgeInsets.symmetric(vertical:10, horizontal: 10),
+            child: Row (
+                children: [
+                  Text('Name:'),
+                  Text('Jael'),
+                ]
             ),
-            Text('are always', style: TextStyle(
-                fontSize: 40,
-                backgroundColor: Colors.pink[300],
-                fontFamily: 'Arial',
-            )),
-            Text('Gorgeous', style: TextStyle(
-                fontSize: 60,
-                backgroundColor: Colors.pink[600],
-                fontFamily: 'Times New Roman',
-                fontWeight: FontWeight.bold,
-            )),
-          ],
-        ),
-    ),
+          ),
+          Container(
+            margin:EdgeInsets.symmetric(vertical:5, horizontal: 10),
+            padding:EdgeInsets.symmetric(vertical:10, horizontal: 10),
+            child: Row (
+                children: [
+                  Text('Age:'),
+                  Text('21 years old'),
+                ]
+            ),
+          ),
+          Container(
+            margin:EdgeInsets.fromLTRB(5, 4, 3, 2),
+            padding:EdgeInsets.symmetric(vertical:10, horizontal: 10),
+            child: Row (
+                children: [
+                  Text('Gender:'),
+                  Text('Female'),
+                ]
+            ),
+          ),
+        ],
+      ),
     ),
   ));
 }
-
