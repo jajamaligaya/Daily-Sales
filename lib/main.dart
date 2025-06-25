90f962f9b57e00a4c6ab52022c2f959497ec3d46
myusername <myemail>
2025-06-25 09:57:43 +0800

    User_Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,124 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(MaterialApp(home: Profile()));
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('DailySales'),
         backgroundColor: Colors.tealAccent[100],
-      ) ,
+      ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            margin:EdgeInsets.all(10),
-            padding:EdgeInsets.symmetric(vertical:10, horizontal: 10),
-            child: Row (
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 30),
+            child: Center(
+              child: Column(
                 children: [
-                  Text('Name:'),
-                  Text('Jael'),
-                ]
+                  Text(
+                    'User Profile',
+                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  ),
+                  Icon(
+                    Icons.person,
+                    size: 100,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
             ),
           ),
           Container(
-            margin:EdgeInsets.symmetric(vertical:5, horizontal: 10),
-            padding:EdgeInsets.symmetric(vertical:10, horizontal: 10),
-            child: Row (
-                children: [
-                  Text('Age:'),
-                  Text('21 years old'),
-                ]
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Name',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text('Jael Maligaya'),
+              ],
             ),
           ),
           Container(
-            margin:EdgeInsets.fromLTRB(5, 4, 3, 2),
-            padding:EdgeInsets.symmetric(vertical:10, horizontal: 10),
-            child: Row (
-                children: [
-                  Text('Gender:'),
-                  Text('Female'),
-                ]
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Email',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text('jael@example.com'),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Company',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text('BSU Balayan'),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Contact Number',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text('09151234567'),
+              ],
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 40),
+              child: Container(
+                width: 300.0,
+                height: 60.0,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.tealAccent.shade100,
+                    foregroundColor: Colors.black,
+                    ),
+                  onPressed: () {
+                    // Button onPressed action
+                  },
+                  child: Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      Icon(Icons.logout),
+                      SizedBox(width:10),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
         ],
       ),
-    ),
-  ));
+    );
+  }
 }
